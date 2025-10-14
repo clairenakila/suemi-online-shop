e290f6ccbe2c02479bb7e1f6a0615ae87159e011
clairenakilacc <clairenakilacc@gmail.com>
2025-10-14 12:31:01 +0800

    .

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+     protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'total_days',
+        'total_hours',
+        'total_daily_pay',
+        'total_overtime_pay',
+        'commission_descriptions',
+        'commission_quantity',
+        'total_commission',
+        'deduction_descriptions',
+        'total_deduction',
+        'gross_pay',
+        'net_pay',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Route::get('/payslips/{user_id}/{start_date}/{end_date}', Payslips::class)
     ->name('payslip.view');
 
+
+
+Route::post('/invoices/create', [Payslips::class, 'storeInvoice'])->name('invoices.create');
